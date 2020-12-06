078cce276350642ed592b07a14b3e715f61d0397
Allan Pozuelos <70873828+Allan1a@users.noreply.github.com>
2020-12-06 11:07:53 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,54 @@
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+## The following function create "matrix" object that will cache its inverse.
+ 
+makeCacheMatrix <- function(x = matrix()) {
+         inv <- NULL
+         set <- function(y) {
+                 x <<- y
+                 inv <<- NULL
+         }
+         get <- function() x
+         setInverse <- function(inverse) inv <<- inverse
+         getInverse <- function() inv
+         list(set = set,
+              get = get,
+              setInverse = setInverse,
+              getInverse = getInverse)
 }
-##
-## Same here, changed "mean" to "solve" and "m" to "s"
+## This function computes the inverse of the "matrix" makeCacheMatrix. 
+##If the inverse has been previously calculated and the matrix has not been changed, the inverse will be retrieved from the cache.
+ 
 cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+         ## Return a matrix that is the inverse of 'x'
+         inv <- x$getInverse()
+         if (!is.null(inv)) {
+                 message("getting cached data")
+                 return(inv)
 }
+         mat <- x$get()
+         inv <- solve(mat, ...)
+         x$setInverse(inv)
+         inv
+}
+## First we can create a "matrix" with the numbers from 1 to 4 and create cache and retrieve the inverse
+
+my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+my_matrix$get()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+my_matrix$getInverse()
+
+## We can then take any random numbers and create cache and retrieve the inverse
+## We will two different "matrices" with randomly chosen numbers
+
+my_matrix <- makeCacheMatrix(matrix(c(105,95,766,83), 2, 2))
+my_matrix$get()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
+
+my_matrix <- makeCacheMatrix(matrix(c(657, 6373, 546, 3), 2, 2))
+my_matrix$get()
+cacheSolve(my_matrix)
+cacheSolve(my_matrix)
+my_matrix$getInverse()
